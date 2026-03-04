02d7f6df5ec36bc76957e578cc542403927370e6
TimWang <tim.wang@pranaq.com>
2026-03-04 14:15:45 +0800

    remove envs

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
     plugins: [react(), tailwindcss()],
-    base: "/piccollege-simple-sudoku/",
-    define: {
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
